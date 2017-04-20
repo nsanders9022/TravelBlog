f2df97994a2a864564ceb1eabbb573fb22a7c390
Cassie Musolf, Keely Glenn and Nicole Sanders <cassie+keely+nicole@>
2017-04-20 13:50:26 -0700

    display the people that belong to each experience

--- src/Travel-Blog/Controllers/ExperiencesController.cs
+++ src/Travel-Blog/Controllers/ExperiencesController.cs
@@ -21,6 +21,12 @@ namespace TravelBlog.Controllers
         public IActionResult Details (int id)
         {
             var thisExperience = db.Experiences.Include(experiences => experiences.Place).FirstOrDefault(experiences => experiences.ExperienceId == id);
+            ViewBag.People = db.Experiences
+                .Include(experience => experience.ExperiencesPeoples)
+                .ThenInclude(experiencesPeoples => experiencesPeoples.People)
+                .Where(experience => experience.ExperienceId == id).ToList();
+                
+
             return View(thisExperience);
         }
         public IActionResult Create()
