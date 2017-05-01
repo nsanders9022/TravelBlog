1c839ad8eac6f0348bb552237d3c3388519f58d3
Nicole Sanders <nsanders9022@gmail.com>
2017-05-01 12:59:07 -0700

    pass unit test for return Index and Index containing a list of Suggestion objects

---
+++ TravelBlog.Tests/ControllerTests/SuggestionsControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Controllers;
+using TravelBlog.Models;
+using Xunit;
+
+namespace TravelBlog.Tests
+{
+    public class SuggestionsControllerTest
+    {
+        //Tests that Index is returned correctly from Controller
+
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            SuggestionsController controller = new SuggestionsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        //Tests that index returns list of Suggestion types
+
+        [Fact]
+        public void Get_ModelList_Index_Test()
+        {
+            //Arrange
+            SuggestionsController controller = new SuggestionsController();
+            IActionResult actionResult = controller.Index();
+            ViewResult indexView = controller.Index() as ViewResult;
+            //ViewResult indexView = new SuggestionsController().Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsType<List<Suggestion>>(result);
+        }
+    }
+}
