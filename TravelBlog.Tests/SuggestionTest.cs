3713c18f4f8f07bc5c47a4f014bf59b4273f061f
Nicole Sanders <nsanders9022@gmail.com>
2017-05-01 12:49:18 -0700

    pass unit test for getDescription

---
+++ TravelBlog.Tests/SuggestionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelBlog.Models;
+using Xunit;
+
+namespace TravelBlog.Tests
+{
+    public class SuggestionTest
+    {
+        public SuggestionTest()
+        {
+        }
+
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var suggestion = new Suggestion();
+            suggestion.Description = "Go hiking and wine tasting";
+
+            //Act
+            var result = suggestion.Description;
+
+            //Assert
+            Assert.Equal("Go hiking and wine tasting", result);
+        }
+    }
+}
